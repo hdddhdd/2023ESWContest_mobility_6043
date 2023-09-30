802ec0226e6a648f7f3f123b55b13ed230527308
wldnd2 <jiwoong000628@gmail.com>
2023-09-30 11:23:55 +0900

    Feat: BBOX(xywh), label, conf 저장 기능 구현

--- AI/detect.py
+++ AI/detect.py
@@ -1,10 +1,9 @@
 import argparse
-import csv
 import os
 import platform
 import sys
 from pathlib import Path
-
+from pprint import pprint
 import torch
 
 FILE = Path(__file__).resolve()
@@ -33,16 +32,12 @@ def run(
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         view_img=False,  # show results
-        save_txt=False,  # save results to *.txt
-        save_csv=False,  # save results in CSV format
-        save_conf=False,  # save confidences in --save-txt labels
         save_crop=False,  # save cropped prediction boxes
         nosave=False,  # do not save images/videos
         classes=None,  # filter by class: --class 0, or --class 0 2 3
         agnostic_nms=False,  # class-agnostic NMS
         augment=False,  # augmented inference
         visualize=False,  # visualize features
-        update=False,  # update all models
         project=ROOT / 'runs/detect',  # save results to project/name
         name='exp',  # save results to project/name
         exist_ok=False,  # existing project/name ok, do not increment
@@ -63,7 +58,7 @@ def run(
 
     # Directories
     save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+    save_dir.mkdir(parents=True, exist_ok=True)  # make dir
 
     # Load model
     device = select_device(device)
@@ -101,6 +96,7 @@ def run(
         with dt[2]:
             pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
 
+        pred_dict = {}
         # Process predictions
         for i, det in enumerate(pred):  # per image
             seen += 1
@@ -112,7 +108,8 @@ def run(
 
             p = Path(p)  # to Path
             # 나중에 필요할 수 있어서 일단 주석 처리
-            # save_path = str(save_dir / p.name)  # im.jpg
+            save_path = str(save_dir / p.name)  # im.jpg
+            
             s += '%gx%g ' % im.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if save_crop else im0  # for save_crop
@@ -131,11 +128,12 @@ def run(
                     c = int(cls)  # integer class
                     label = names[c] if hide_conf else f'{names[c]}'
                     confidence = float(conf)
-                    confidence_str = f'{confidence:.2f}'
-
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                    box = xyxy2xywh(torch.tensor(xyxy).view(1, 4)).view(-1).tolist()
+                    # print(label, confidence, box)
+                    if label not in pred_dict:
+                        pred_dict[label] = [[confidence, box]]
+                    else:
+                        pred_dict[label].append([confidence, box])
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
@@ -155,24 +153,24 @@ def run(
                 cv2.waitKey(1)  # 1 millisecond
 
             # 나중에 필요할 수 있어서 일단 주석 처리
-            # # Save results (image with detections)
-            # if save_img:
-            #     if dataset.mode == 'image':
-            #         cv2.imwrite(save_path, im0)
-            #     else:  # 'video' or 'stream'
-            #         if vid_path[i] != save_path:  # new video
-            #             vid_path[i] = save_path
-            #             if isinstance(vid_writer[i], cv2.VideoWriter):
-            #                 vid_writer[i].release()  # release previous video writer
-            #             if vid_cap:  # video
-            #                 fps = vid_cap.get(cv2.CAP_PROP_FPS)
-            #                 w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-            #                 h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            #             else:  # stream
-            #                 fps, w, h = 30, im0.shape[1], im0.shape[0]
-            #             save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
-            #             vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-            #         vid_writer[i].write(im0)
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'image':
+                    cv2.imwrite(save_path, im0)
+                else:  # 'video' or 'stream'
+                    if vid_path[i] != save_path:  # new video
+                        vid_path[i] = save_path
+                        if isinstance(vid_writer[i], cv2.VideoWriter):
+                            vid_writer[i].release()  # release previous video writer
+                        if vid_cap:  # video
+                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        else:  # stream
+                            fps, w, h = 30, im0.shape[1], im0.shape[0]
+                        save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
+                        vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    vid_writer[i].write(im0)
 
         # Print time (inference-only)
         LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
@@ -180,11 +178,10 @@ def run(
     # Print results
     t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
     LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
-    if update:
-        strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
+    if save_img:
+        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}")
+    print("********* RESULT *********")
+    pprint(pred_dict)
 
 def detection(model, input):
     args = argparse.Namespace(
@@ -198,14 +195,12 @@ def detection(model, input):
         max_det=1000,
         device='',
         view_img=False,
-        save_txt=False,
         save_crop=False,
         nosave=False,
         classes=None,
         agnostic_nms=False,
         augment=False,
         visualize=False,
-        update=False,
         project='runs/detect',
         name='exp',
         exist_ok=False,
@@ -221,4 +216,4 @@ def detection(model, input):
 if __name__ == '__main__':
     # detection("last.pt", "test.jpg")
     # detection("last.pt", "http://127.0.0.1:5000/video_feed")
-    detection("yolov5n.pt", "0")
+    detection("yolov5n.pt", "input.png")
