d17e4abb57400a8582c5a25c0b8d8395630728ab
dongju333 <dongjukim5129@gmail.com>
2023-10-02 22:39:47 +0900

    Refactor:  충격 감지센서 모듈화 코드 변경

--- Sensor/SW420.py
+++ Sensor/SW420.py
@@ -1,32 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+def Crash_D():
+    GPIO.setmode(GPIO.BCM)
 
-PIN_NUM = 24
-LED = 26
-CHECK_ON = 1
+    PIN_NUM = 24
+    CHECK_ON = 1
 
-GPIO.setup(PIN_NUM, GPIO.IN)
-GPIO.setup(LED, GPIO.OUT)
+    GPIO.setup(PIN_NUM, GPIO.IN)
 
-PREV_TIME=time.time()
-CUR_TIME=time.time()
-
-try:
-    i=0
-    while True:
-        CUR_TIME=time.time()
-        if GPIO.input(PIN_NUM)==CHECK_ON:
-            i=i+1
-        if CUR_TIME-PREV_TIME > 5:
-            if i > 20:
-                GPIO.output(LED,GPIO.HIGH)
-            else:
-                GPIO.output(LED,GPIO.LOW)
-            PREV_TIME=time.time()
-            print("COUNT: ",i)
-            i=0
-        time.sleep(0.01)
-finally:
+    if GPIO.input(PIN_NUM)==CHECK_ON:
+        GPIO.cleanup()
+        return "충돌이 감지되었습니다."
     GPIO.cleanup()
+    return "평상시"
