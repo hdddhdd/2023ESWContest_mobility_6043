4ea696913e304a7e0f0c397ca47052270dfc8f57
wldnd2 <jiwoong000628@gmail.com>
2023-10-09 11:02:21 +0900

    Refactor: 파이어베이스 데이터 영상 불러오기

--- src/pages/VideoPage.jsx
+++ src/pages/VideoPage.jsx
@@ -1,36 +1,40 @@
 import { useEffect, useState } from 'react';
-import { ref, get, child } from 'firebase/database'; // Firebase Realtime Database 관련 import
-import {realtimeDbService} from '../utils/firebase';
+import { ref, get, child } from 'firebase/database';
+import { realtimeDbService } from '../utils/firebase';
 import VideoPlayer from '../components/VideoPlayer';
 
 export default function VideoPage() {
-  const [data, setData] = useState(null); // 데이터를 저장할 상태 변수
+  const [data, setData] = useState([]); // 데이터를 저장할 상태 변수
 
-  const readOne = () => {
+  useEffect(() => {
     const dbRef = ref(realtimeDbService);
-    get(child(dbRef, '/test'))
-      .then(snapshot => {
+
+    get(child(dbRef, 'test'))
+      .then((snapshot) => {
         if (snapshot.exists()) {
-          setData(snapshot.val()); // 데이터를 상태 변수에 저장
+          const newData = [];
+          snapshot.forEach((childSnapshot) => {
+            // 각 데이터 항목을 배열에 추가
+            newData.push(childSnapshot.val());
+          });
+          setData(newData); // 데이터를 상태 변수에 저장
         } else {
           console.log('No data available');
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
-  };
-
-  useEffect(() => {
-    readOne(); // 페이지가 로딩될 때 함수 실행
   }, []);
 
   return (
     <div>
-      {data ? (
+      {data.length > 0 ? (
         // 데이터가 있는 경우 출력
         <div>
-          <VideoPlayer videoUrl = {data.image_url}/>
+          {data.map((item, index) => (
+            <VideoPlayer key={index} videoUrl={item.image_url} />
+          ))}
         </div>
       ) : (
         // 데이터가 없는 경우 출력
