f2722f61f7f52074147a71666e86c680de01e87d
wldnd2 <jiwoong000628@gmail.com>
2023-10-09 11:10:30 +0900

    Refactor: 비디오 형식 변경 및 DB 형식 변경

--- Server/app.py
+++ Server/app.py
@@ -11,14 +11,16 @@ from flask import Flask, render_template, Response
 # 센서 모듈 불러오기
 from MPU6050 import MPU
 from SW420 import Crash_D 
+from MP3Player import player
 
 app = Flask(__name__)
 capture = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
+cv2.VideoWriter_fourcc(*'mp4v')
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 font = ImageFont.truetype('fonts/SCDream6.otf', 20)
 # 전역 변수 설정
+DB_name = ""
 is_record = False
 start_record = False
 video = None
@@ -29,39 +31,40 @@ def database_init():
     try:
         app = firebase_admin.get_app()
     except ValueError as e:
-        cred = credentials.Certificate("privateKey.json")
+        cred = credentials.Certificate("whycarno-firebase-firebase-adminsdk-e0i3x-cce4eba255.json")
         firebase_admin.initialize_app(cred, {
             "databaseURL": f"https://{cred.project_id}-default-rtdb.firebaseio.com/",
             "storageBucket": f"{cred.project_id}.appspot.com"
         })
 
-def firebase_process(image_file_path):
+def firebase_process(image_file_path, DB_name):
     folder_path, image_file_name = image_file_path.split('/')[0], image_file_path.split('/')[1]
     bucket = storage.bucket()
     new_token = uuid4()
     metadata = {"firebaseStorageDownloadTokens": new_token}
     blob = bucket.blob(image_file_path)
-    blob.content_type = 'video/avi'
+    blob.content_type = 'video/mp4'
     blob.metadata = metadata
     blob.upload_from_filename(image_file_path)
+    # 날짜 - 시간
     update_data = {
-        "date_time": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        "image_url": f"https://firebasestorage.googleapis.com/v0/b/{bucket.name}/o/{folder_path}%2F{image_file_name}?alt=media&token={new_token}"
+        "accident_time": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        "video_url": f"https://firebasestorage.googleapis.com/v0/b/{bucket.name}/o/{folder_path}%2F{image_file_name}?alt=media&token={new_token}"
     }
-    dir = db.reference("test")
+    dir = db.reference(f"accident/{DB_name}")
     dir.update(update_data)
-    ref = db.reference("test")
+    ref = db.reference("accident")
     return ref.get()
 
 def gen_frames():  
     database_init()
-    global is_record, start_record, video, fourcc
+    global is_record, start_record, video, fourcc, DB_name
     # 현재 작업 디렉토리에 "result" 디렉토리가 없으면 생성
     directory = "result"
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    video_name = f"{directory}/accident.avi"  # 파일 경로로 설정
+    video_name = f"{directory}/accident.mp4"  # 파일 경로로 설정
     while True:
         now = datetime.datetime.now()
         nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
@@ -78,23 +81,25 @@ def gen_frames():
             frame = buffer.tobytes()
             # 자이로 센서 값 받아오기
             start_record, value = MPU()
-            if start_record: 
+            if start_record:
                 if is_record == False:     # 녹화 시작
                     if os.path.exists(video_name):
                         os.remove(video_name)
                         print("REMOVED!!!")
                         time.sleep(2)
                     print("//////// Recording Start ////////", value)
+                    player(False, "mp3/turn_right.mp3")
+                    DB_name = datetime.datetime.now().strftime('%Y-%m-%d')
                     is_record = True
                     start_record = False
                     video = cv2.VideoWriter(video_name, fourcc, 15, (frame1.shape[1], frame1.shape[0]))
                 else:      # 녹화 중
-                    print("+++++++++ RECORDING!!! ++++++++++++")
+                    print("+++++++++ RECORDING!!! ++++++++++++", value)
                     if Crash_D():
                         print("***** CRASH!!!! *****")
                         is_record = False
                         start_record = False
-                        result = firebase_process(video_name)
+                        result = firebase_process(video_name, DB_name)
                         print(result)
                         print("=== Recording Stop and DB upload Success ===")
                         # reuslt 파일 삭제
