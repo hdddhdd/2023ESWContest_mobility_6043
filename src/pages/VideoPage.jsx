ffb34185c0115fd6ccb20a74479ad480d20d75a1
wldnd2 <jiwoong000628@gmail.com>
2023-10-09 11:56:17 +0900

    Style: 비디오 페이지 스타일 적용

--- src/pages/VideoPage.jsx
+++ src/pages/VideoPage.jsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react';
 import { ref, get, child } from 'firebase/database';
 import { realtimeDbService } from '../utils/firebase';
 import ReactPlayer from 'react-player';
+import './VideoPage.css'; // CSS 모듈 가져오기
 
 export default function VideoPage() {
-  const [data, setData] = useState([]); // 데이터를 저장할 상태 변수
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const dbRef = ref(realtimeDbService);
@@ -14,10 +15,9 @@ export default function VideoPage() {
         if (snapshot.exists()) {
           const newData = [];
           snapshot.forEach((childSnapshot) => {
-            // 각 데이터 항목을 배열에 추가
             newData.push(childSnapshot.val());
           });
-          setData(newData); // 데이터를 상태 변수에 저장
+          setData(newData);
         } else {
           console.log('No data available');
         }
@@ -28,30 +28,25 @@ export default function VideoPage() {
   }, []);
 
   return (
-    <div>
+    <div className='DB-container'>
       {data.length > 0 ? (
-        // 데이터가 있는 경우 출력
-        console.log(JSON.stringify(data, null, 4)),
         <div>
           {data.map((timeData, index) => (
-            <div key={index}>
-                {Object.keys(timeData).map((time, subIndex) => (
-                    <div key={subIndex}>
-                      <h3>{time}</h3>
-                      <p>Date & Time: {timeData[time].date_time}</p>
-                      <ReactPlayer
-                        url={timeData[time].video_Url}
-                        controls // 동영상 컨트롤러 표시
-                        width="700px" // 동영상 너비 조절
-                        height="500px" // 동영상 높이 자동 조절
-                      />
-                    </div>
-                ))}
+            <div key={index} className='video-container'>
+              {Object.keys(timeData).map((time, subIndex) => (
+                <div key={subIndex} className='object-contianer'>
+                  <p>Date : {timeData[time].date_time}</p>
+                  <ReactPlayer
+                    url={timeData[time].video_Url}
+                    controls
+                    className="video"
+                  />
+                </div>
+              ))}
             </div>
           ))}
         </div>
       ) : (
-        // 데이터가 없는 경우 출력
         <p>Loading...</p>
       )}
     </div>
