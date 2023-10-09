7a0494d342fc0d4b3f6f85fcdd5dd506cdcd389a
wldnd2 <jiwoong000628@gmail.com>
2023-10-09 14:32:46 +0900

    Style: 버튼 스타일 업데이트

--- src/components/ControlButton.jsx
+++ src/components/ControlButton.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import { BsPlay, BsPower } from 'react-icons/bs';
+import { BsFillBellSlashFill, BsFillBellFill, BsFillCaretRightFill } from 'react-icons/bs';
 import {Link} from 'react-router-dom';
 import "./ControlButton.css";
 
@@ -10,10 +10,10 @@ function ControlButton() {
   const handleCollisionTTSClick = () => {
     // 충돌감지 TTS 상태 토글
     if (isCollisionTTSOn) {
-      alert('알림을 끕니다.');
+      alert('알림을 켭니다.');
       //서버로 버튼 값 넘겨서 음소거
     } else {
-      alert('알림을 켭니다.');
+      alert('알림을 끕니다.');
       //서버로 버튼 값 넘겨서 다시 소리 들리게
     }
     setIsCollisionTTSOn(!isCollisionTTSOn);
@@ -22,12 +22,12 @@ function ControlButton() {
   return (
     <div className="App">
       <Link to="/video">
-        <button className="icon-button play-button">
-          {<BsPlay className="icon" />} 녹화 영상
+        <button className="play-button">
+          {<BsFillCaretRightFill className="icon" />} 영상
         </button>
       </Link>
       <button className={`icon-button ${isCollisionTTSOn ? 'on-button' : 'off-button'}`} onClick={handleCollisionTTSClick}>
-        {isCollisionTTSOn ? <BsPower className="icon" /> : <BsPower className="icon" />} 알림{isCollisionTTSOn ? '끄기' : '켜기'}
+        {isCollisionTTSOn ? <BsFillBellSlashFill className="icon" /> : <BsFillBellFill className="icon" />} 알림
       </button>
     </div>
   );
