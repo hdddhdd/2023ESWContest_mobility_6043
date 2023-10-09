18a0762f62f710cec0bf60c3e019957e130f4098
wldnd2 <jiwoong000628@gmail.com>
2023-10-09 11:29:24 +0900

    Refactor: DB 수정 및 read 데이터 수정

--- src/pages/VideoPage.jsx
+++ src/pages/VideoPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { ref, get, child } from 'firebase/database';
 import { realtimeDbService } from '../utils/firebase';
-import VideoPlayer from '../components/VideoPlayer';
+import ReactPlayer from 'react-player';
 
 export default function VideoPage() {
   const [data, setData] = useState([]); // 데이터를 저장할 상태 변수
@@ -9,7 +9,7 @@ export default function VideoPage() {
   useEffect(() => {
     const dbRef = ref(realtimeDbService);
 
-    get(child(dbRef, 'test'))
+    get(child(dbRef, 'mydata'))
       .then((snapshot) => {
         if (snapshot.exists()) {
           const newData = [];
@@ -31,9 +31,23 @@ export default function VideoPage() {
     <div>
       {data.length > 0 ? (
         // 데이터가 있는 경우 출력
+        console.log(JSON.stringify(data, null, 4)),
         <div>
-          {data.map((item, index) => (
-            <VideoPlayer key={index} videoUrl={item.image_url} />
+          {data.map((timeData, index) => (
+            <div key={index}>
+                {Object.keys(timeData).map((time, subIndex) => (
+                    <div key={subIndex}>
+                      <h3>{time}</h3>
+                      <p>Date & Time: {timeData[time].date_time}</p>
+                      <ReactPlayer
+                        url={timeData[time].video_Url}
+                        controls // 동영상 컨트롤러 표시
+                        width="700px" // 동영상 너비 조절
+                        height="500px" // 동영상 높이 자동 조절
+                      />
+                    </div>
+                ))}
+            </div>
           ))}
         </div>
       ) : (
