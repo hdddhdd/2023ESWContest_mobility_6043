4d1a304734a4d8f623ed458b5c8e3bfe62345863
wldnd2 <jiwoong000628@gmail.com>
2023-10-14 18:30:50 +0900

    Feat: 음소거 버튼 활성화 기능 구현

--- src/components/ControlButton.jsx
+++ src/components/ControlButton.jsx
@@ -12,9 +12,28 @@ function ControlButton() {
     if (isCollisionTTSOn) {
       alert('알림을 켭니다.');
       //서버로 버튼 값 넘겨서 음소거
+      console.log("fetch");
+      fetch("http://127.0.0.1:8080/player").then(
+        // response 객체의 json() 이용하여 json 데이터를 객체로 변화
+        res => res.json()
+      ).then(
+        // 데이터를 콘솔에 출력
+        data => console.log(data)
+      )
+      console.log("fetch end");
     } else {
       alert('알림을 끕니다.');
       //서버로 버튼 값 넘겨서 다시 소리 들리게
+      //서버로 버튼 값 넘겨서 음소거
+      console.log("fetch");
+      fetch("http://127.0.0.1:8080/player").then(
+        // response 객체의 json() 이용하여 json 데이터를 객체로 변화
+        res => res.json()
+      ).then(
+        // 데이터를 콘솔에 출력
+        data => console.log(data)
+      )
+      console.log("fetch end");
     }
     setIsCollisionTTSOn(!isCollisionTTSOn);
   };
